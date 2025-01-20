eb5b87500cda67b54d37863b449411425dc96c79
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2025-01-20 23:21:06 +0530

    Dashboard Screen | Added background presss button to avoid unwanted logout to new logged in users | --Fixed

--- app/(needAuth)/(protected)/Dashboard.tsx
+++ app/(needAuth)/(protected)/Dashboard.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useCallback } from 'react'
+import { useFocusEffect } from 'expo-router';
+import { Alert, BackHandler, StyleSheet, View } from 'react-native'
 
 import MapAndStatsHolder from '@/components/designs/dashboard/MapAndStatsHolder';
 import BottomButtonHolder from '@/components/designs/dashboard/BottomButtonHolder';
 import DashboardUserInformation from '@/components/designs/dashboard/DashboardUserInformation';
 
 const Dashboard = () => {
+
+  useFocusEffect(
+    useCallback(() => {
+      const onBackPress = (): boolean => {
+        Alert.alert("Hold on!", "Are you sure you want to go back?", [
+          {
+            text: "Cancel",
+            onPress: () => null,
+            style: "cancel",
+          },
+          { text: "YES", onPress: () => BackHandler.exitApp() },
+        ]);
+        return true;
+      };
+
+      BackHandler.addEventListener("hardwareBackPress", onBackPress);
+
+
+      return () =>
+        BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+    }, [])
+  );
+
   return (
     <View style={styles.outerContainer}>
 

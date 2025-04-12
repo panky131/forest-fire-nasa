e4d3527ef744f064d237ca9dac861823825808a4
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2025-04-12 17:02:31 +0530

    Removed unwanted api urls

--- utils/URLs.ts
+++ utils/URLs.ts
@@ -1,7 +1,5 @@
 const URLs = {
-  // api_base_url: "https://www.buizhelp.com/forest_fire_uttarakhand/api/"
   api_base_url: "https://forestfireuttarakhand.in/admin/api/"
-  // api_base_url: "https://winhillglobalschool.com/forest_fire_uk/"
 };
 
 export default URLs;
